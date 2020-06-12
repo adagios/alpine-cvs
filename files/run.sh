7fec3d4583d07434fd2c9cc22cbafed675a87ffc
João Nelas <joao.nelas@gmail.com>
2020-06-12 10:03:39 +0100

    creates system user to own the cvsroot
    Otherwise it fails with "no such system user" when  doing `cvs login`

--- files/run.sh
+++ files/run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
+adduser -D -H $CVS_USER
+
 cvs -d /var/cvsroot init
 echo $CVS_USER':'$(openssl passwd -crypt $CVS_PASSWD) > /var/cvsroot/CVSROOT/passwd
+
+CHOWN -R $CVS_USER /var/cvsroot
 
 inetd -fe
