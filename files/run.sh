66b6f957dbf6be2be543d63a0783f76971e5c6b3
João Nelas <joao.nelas@gmail.com>
2020-06-15 15:53:53 +0100

    correct capitalization

--- files/run.sh
+++ files/run.sh
@@ -5,6 +5,6 @@ adduser -D -H $CVS_USER
 cvs -d /var/cvsroot init
 echo $CVS_USER':'$(openssl passwd -crypt $CVS_PASSWD) > /var/cvsroot/CVSROOT/passwd
 
-CHOWN -R $CVS_USER /var/cvsroot
+chown -R $CVS_USER /var/cvsroot
 
 inetd -fe
